4d72cdb91a2ace80d7c16873e5c0494853a0d907
liuchuo <95323362@qq.com>
2015-06-23 23:22:46 +0800

    循环队列基本操作

--- 20150623-1/20150623-1/main.c
+++ 20150623-1/20150623-1/main.c
@@ -16,21 +16,50 @@ typedef struct Squeue{
     int front,rear;
 }SeqQueue;
 
-
-
 //初始化队列
 void InitQueue(SeqQueue *SCQ){
     SCQ->front = SCQ->rear = 0;
 }
 
-
-
 //判断队列是否为空
 int QueueEmpty(SeqQueue SCQ){
-    if(SCQ.front == SCQ.rear){
+    if(SCQ.front == SCQ.rear)
+        return 1;
+    else
+        return 0;
+}
+
+//将队列e入队
+int EnQueue(SeqQueue *SCQ,DataType e){
+    if(SCQ->front == (SCQ->rear + 1) % QueueSize)
+        return 0;
+    SCQ->queue[SCQ->rear] = e;
+    SCQ->rear = (SCQ->rear + 1) % QueueSize;
+    return 1;
+}
+
+//将队头出队
+int DeQueue(SeqQueue *SCQ,DataType *e){
+    if(SCQ->front == SCQ->rear)
+        return 0;
+    else{
+        *e = SCQ->queue[SCQ->front];
+        SCQ->front = (SCQ->front + 1) % QueueSize;
         return 1;
     }
-    else{
+}
+
+//取队头元素
+int GetHead(SeqQueue SCQ,DataType *e){
+    if(SCQ.front == SCQ.rear)
         return 0;
+    else{
+        *e = SCQ.queue[SCQ.front];
+        return 1;
     }
+}
+
+//清空队列
+void ClearQueue(SeqQueue *SCQ){
+    SCQ->front = SCQ->rear = 0;
 }
