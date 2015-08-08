8aed499b7be4d24a2c39ccd8120ea3c1357ab8d1
liuchuo <95323362@qq.com>
2015-08-08 18:41:10 +0800

    可变参数...va_list,va_arg,va_start,va_end

--- 愿得韶华刹那，开得满树芳华/愿得韶华刹那，开得满树芳华/main.c
+++ 愿得韶华刹那，开得满树芳华/愿得韶华刹那，开得满树芳华/main.c
@@ -6,10 +6,12 @@
 //  Copyright (c) 2015年 欣 陈. All rights reserved.
 //
 
-//数组的存储结构、及其基本运算
+///数组的存储结构、及其基本运算
 #define MaxArraySize 3
 typedef int DataType;
-#include <stdarg.h>//stdarg.h 头文件定义了一个变量类型 va_list 和三个宏，这三个宏可用于在参数个数未知（即参数个数可变）时获取函数中的参数。可变参数的函数通在参数列表的末尾是使用省略号(,...)定义的
+#include <stdarg.h>//stdarg.h 头文件定义了一个变量类型
+//va_list 和三个宏，这三个宏可用于在参数个数未知（即参数个数可变）时获取函数中的参数。
+//可变参数的函数通在参数列表的末尾是使用省略号(,...)定义的
 #include <stdlib.h>
 typedef struct {
     DataType *base;
@@ -18,7 +20,11 @@ typedef struct {
     int *constants; //数组存储映像常量基地址
 }Array;
 
-int InitArray(Array *A,int dim,...) {
+int InitArray(Array *A,int dim,...) {/*使用变长参数表传递参数，
+                                      即用...表示形式参数，这种变长的形式参数主要用于参数不确定的情况，
+                                      数组可能是二维的，也可能是三维的，参数的个数也就会不同，在这种情况下，
+                                      就需要在被调用函数的形式参数列表中使用变长形式参数。在函数的形式参数表中，
+                                      至少要有一个固定的参数在变长的形式参数表的前面。*/
     int elemtotal = 1; //elemtotal是数组元素总数，初值为1
     int i;
     va_list ap;
@@ -28,35 +34,37 @@ int InitArray(Array *A,int dim,...) {
     A->bounds = (int *)malloc(dim * sizeof(int));
     if(!A->bounds)
         exit(-1);
-        va_start(ap,dim);
-        for(i = 0;i < dim;i++){
-            A->bounds[i] = va_arg(ap,int);
-            if(A->bounds[i] < 0)
-                return -1;
-            elemtotal *= A->bounds[i];
-        }
+    va_start(ap,dim);
+    for(i = 0;i < dim;i++){
+        A->bounds[i] = va_arg(ap,int);
+        if(A->bounds[i] < 0)
+            return -1;
+        elemtotal *= A->bounds[i];
+    }
     va_end(ap);
     A->base = (DataType *)malloc(elemtotal * sizeof(DataType));
     if(!A->base)
         exit(-1);
-        A->constants = (int *)malloc(dim * sizeof(int));
-        if(!A->constants)
-            exit(-1);
-            A->constants[dim - 1] = 1;
-            for(i = dim -2;i >= 0;--i)
-                A->constants[i] = A->bounds[i + 1] * A->constants[i + 1];
-                return 1;
+    A->constants = (int *)malloc(dim * sizeof(int));
+    if(!A->constants)
+        exit(-1);
+    A->constants[dim - 1] = 1;
+    for(i = dim -2;i >= 0;--i)
+        A->constants[i] = A->bounds[i + 1] * A->constants[i + 1];
+    return 1;
 }
 
-/*
- 1	void va_start(va_list ap, last_arg)
- 这个宏初始化 ap 变量，它与 va_arg 和 va_end 宏是一起使用的。last_arg 是最后一个传递给函数的已知的固定参数，即省略号之前的参数。
- 2	type va_arg(va_list ap, type)
- 这个宏检索函数参数列表中类型为 type 的下一个参数。
- 3	void va_end(va_list ap)
- 这个宏允许使用了 va_start 宏的带有可变参数的函数返回。如果在从函数返回之前没有调用 va_end，则结果为未定义。
- */
+//可变参数标准宏头文件
+#include "stdarg.h"
 
+//申明va_list数据类型变量ap，该变量访问变长参数列表中的参数。即定义了一个可变参数的指针ap
+va_list ap;
+//调用va_start(ap,dim)使ap指向dim的下一个参数，即第一个变长参数
+va_start(ap,dim);
+//然后调用va_arg(ap,int)返回可变参数的值
+va_arg(ap,type);
+//关闭本次对变长参数列表的访问。
+va_end(ap);
 
 //数组定位
 int LocateArray(Array A,va_list ap,int *offset){
