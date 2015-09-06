dc89ddbaeb12f84d66135d4e8435e0c97dfca1a3
liuchuo <95323362@qq.com>
2015-09-06 21:39:45 +0800

    。。。

---
+++ w232/w232/main.c
@@ -0,0 +1,32 @@
+//
+//  main.c
+//  w232
+//
+//  Created by 欣 陈 on 15/9/6.
+//  Copyright (c) 2015年 欣 陈. All rights reserved.
+//
+
+#include <stdio.h>
+int min(int *s,int n) {
+    int i;
+    int flag,hehe;
+    flag = -1;
+    int f = 100;
+    for(i = 0;i < n;i++) {
+        if(s[i] < f && i != hehe) {
+             f = s[i];
+            flag = i;
+        }
+    }
+    hehe = flag;
+    return flag;
+}
+
+int main() {
+    int s[8] = {5,3,4,1,0,-2,-1,8};
+    int s1,s2;
+    s1 = min(s,8);
+    s2 = min(s,8);
+    printf("s1=%d,s2=%d",s1,s2);
+    return 0;
+}
