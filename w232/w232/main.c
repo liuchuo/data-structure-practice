a1f986fa2f3bdce2662005f2284ad58cdb0b5718
liuchuo <95323362@qq.com>
2015-09-11 22:56:41 +0800

    。。。

--- w232/w232/main.c
+++ w232/w232/main.c
@@ -6,7 +6,7 @@
 //  Copyright (c) 2015年 欣 陈. All rights reserved.
 //
 
-#include <stdio.h>
+//#include <stdio.h>
 //int min(int *s,int n) {
 //    int i;
 //    int flag,hehe;
@@ -30,12 +30,85 @@
 //    printf("s1=%d,s2=%d",s1,s2);
 //    return 0;
 //}
+//
+//int main() {
+//    int i;
+//    i = 1;
+//    for(;i <= 5;++i) {
+//        printf("%d\t",i);
+//    }
+//    return 0;
+//}
+#include <iostream>
+#include <cstdlib>
 
-int main() {
-    int i;
-    i = 1;
-    for(;i <= 5;++i) {
-        printf("%d\t",i);
+using namespace std;
+
+typedef struct Node{
+    int element;
+    struct  Node * next;
+}Node;
+
+//Initialize a link list.
+Node * Init(Node * head){
+    if(head == NULL){
+        head = null;
     }
-    return 0;
+    
+    head = (Node * ) malloc (sizeof (Node));
+    return head;
+}
+
+//DestoryList a link list.
+void Drop(Node * head){
+    Node * p = head;
+    while(p->next){
+        head = p;
+        p = p->next;
+        free(head);
+    }
+}
+
+//Clear a link list.
+void Clear(Node * head){
+    if(head->next){
+        drop(head->next);
+    }
+}
+
+//Link list whether is empty.
+bool Empty(Node * head){
+    return (head->next == 0)
+}
+
+//Get the length of the link list.
+int Length(Node * head){
+    Ndoe * p = head;
+    int len = 0;
+    while(p->next){
+        len++;
+        p = p->next;
+    }
+    return len;
+}
+
+/*
+ //Find a element by index.
+ int GetElement(Node * head, int index){
+	
+	if(index < 0 && index >= Length(head)){
+ cout << "You input index are over flow" << endl;
+	}
+ 
+	Node * p = head;
+	int i = 0;
+	while(i <= index && p){
+ 
+	}
+ }
+ */
+
+int main(){
+    Node * head;
+    head = init(head);
 }
